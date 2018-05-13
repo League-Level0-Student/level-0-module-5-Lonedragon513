1ac6be37229d0930a406da6062cc1f2bfb6c0ec3
league <league@wts14.attlocal.net>
2018-05-12 17:27:48 -0700

    qWERTYUIASDF

--- src/Code4Life.java
+++ src/Code4Life.java
@@ -1,38 +1,37 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
  */
 
-import java.io.File;
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JOptionPane;
 
-
 public class Code4Life {
 	public static void main(String[] args) {
-		
-	//1.  Ask the user how many hours they spent coding this week.
+
+		// 1. Ask the user how many hours they spent coding this week.
 	}
+
 	String A = JOptionPane.showInputDialog("how many hour");
-	//2.  If it is less than or equal to 2, tell them to stop watching YouTube and write code instead.
-	
-	//3.  If it is greater than or equal to 3 AND less than or equal to 5, tell them they're a Code Ninja
-	
-	//4.  If it is more than 5, call the method below to play the Batman theme song.  
+	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
+	// write code instead.
 
-    private static void playBatmanTheme() {
-    	try {
-   		 Clip clip = AudioSystem.getClip();
-   		 clip.open(AudioSystem.getAudioInputStream(Code4Life.class.getResource("batman.wav")));
-   		 clip.start();
-   		 Thread.sleep(60002);
-   	} catch (Exception ex) {
-     	ex.printStackTrace();
-   	}
-    }
+	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
+	// they're a Code Ninja
 
+	// 4. If it is more than 5, call the method below to play the Batman theme song.
 
-}
+	private static void playBatmanTheme() {
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(Code4Life.class.getResource("batman.wav")));
+			clip.start();
+			Thread.sleep(60002);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
 }
--- src/loop.java
+++ src/loop.java
@@ -2,7 +2,8 @@ import javax.swing.JOptionPane;
 
 public class loop {
 	public static void main(String[] args) {
-		numberg();
+		Bb();
+
 	}
 
 	public static void numberc() {
@@ -45,18 +46,76 @@ public class loop {
 			System.out.println(i + 1 + " is even");
 		}
 	}
-	
+
 	public static void numberg() {
 		for (int i = 1; i < 777; i++) {
-			if ( i%7==0) {
+			if (i % 7 == 0) {
 				System.out.println(i);
 			}
 		}
 	}
-	
+
 	public static void numberh() {
 		String a = JOptionPane.showInputDialog("how old");
-		
-		for (int i = 1; i < 777; i++) {
+		int b = Integer.parseInt(a);
+		int c = 2018;
+		for (int i = 0; i < b; i++) {
+			System.out.println("in " + (c - i) + " you are " + (b - i));
+		}
 	}
+
+	public static void numbernotworking() {
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		for (int m = 0; m < 1; m++) {
+			for (int i = 0; i < 3; i++) {
+
+				System.out.println(a + " " + b + " " + c);
+				a = a + 3;
+				b = b + 3;
+				c = c + 3;
+			}
+		}
+	}
+
+	public static void hii() {
+		for (int m = 0; m < 3; m++) {
+			for (int x = 1; x < 4; x++) {
+				System.out.print(x + 3 * m);
+				System.out.print("   ");
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+	public static void IcanFly() {
+		for (int m = 0; m < 10; m++) {
+			for (int x = 1; x < 11; x++) {
+				System.out.print(x + 10 * m);
+				System.out.print("   ");
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+	public static void Iis() {
+		for (int m = 0; m < 6; m++) {
+			for (int x = 0; x < m; x++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+	public static void Bb() {
+		for (int m = 0; m < 101; m++) {
+			System.out.println(100 - m);
+		}
+
+	}
+
 }
